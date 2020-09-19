164ec293b9f5203f916aac0232861f8962c6cdb1
Wilma <kima_92@fastmail.com>
2020-09-19 15:24:42 -0400

    Setting up the ImagePicker

--- Pet Recognizer/View Controllers/ViewController.swift
+++ Pet Recognizer/View Controllers/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - DidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupImagePicker()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        present(imagePicker, animated: true)
+        // ^^ for some reazon I cant present it inside viewDidLoad
+    }
+    
+    // MARK: - Methods
+    
+    func setupImagePicker() {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        imageView.image = userPickedImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
 
