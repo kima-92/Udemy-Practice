c539aa34854d22dda8a8e55a5f212738be0aab8a
Wilma <kima_92@fastmail.com>
2020-09-19 09:52:06 -0400

    ImagePicker setup

--- SeeFood/View Controllers/ViewController.swift
+++ SeeFood/View Controllers/ViewController.swift
@@ -20,10 +20,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     // MARK: - DidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false  // Give true a try in your next app
     }
     
     // MARK: - Action
     @IBAction func cameraBarButtonTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - Methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Getting the image from the camera
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        imageView.image = userPickedImage
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 }
 
