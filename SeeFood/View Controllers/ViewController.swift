d13dbfbe20b8252dea976432ac1b234c3cda4274
Wilma <kima_92@fastmail.com>
2020-09-19 12:50:05 -0400

    Classifying an Image using Inceptionv3's model

--- SeeFood/View Controllers/ViewController.swift
+++ SeeFood/View Controllers/ViewController.swift
@@ -40,6 +40,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imageView.image = userPickedImage
         imagePicker.dismiss(animated: true, completion: nil)
+        
+        // Create a Core Image Image which is what CoreML uses
+        guard let ciImage = CIImage(image: userPickedImage) else {
+            fatalError("Couldn't convert image to a CIImage")
+        }
+        detect(image: ciImage)
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Try to create a model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Failed loading CoreML Model")
+        }
+        
+        // Create a Request
+        // which will return an array of classifications of the given image
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            // Request an array of classifications of
+            // An array of ClassificationObservations AFTER the image has been processed
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        // Try to perform the request with this handler
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 }
 
