f92a27f16df5918f58f2d3c5629ea6d955a38e91
Wilma <kima_92@fastmail.com>
2020-09-19 14:49:14 -0400

    Using CreateML to create and train PetRecognizer.mlmodel

---
+++ Pet Recognizer/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pet Recognizer
+//
+//  Created by macbook on 9/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
